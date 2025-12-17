62831f0e23c357980e9e9c4acccc29108ac681e5
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-12-17 22:40:14 +0100

    create authentication utils

--- app/authentication/auth.py
+++ app/authentication/auth.py
@@ -1,7 +1,6 @@
-import os, jwt
-from dotenv import load_dotenv
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+import jwt
+from fastapi import Depends, HTTPException, status, Request
+from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 from ..models.user_model import User
 from ..schemas.user_schema import TokenData
@@ -10,12 +9,7 @@ from sqlalchemy.orm import Session
 from datetime import timedelta, datetime, timezone
 from typing import Optional
 from jwt.exceptions import InvalidTokenError
-
-load_dotenv()
-
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+from ..config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_schema = OAuth2PasswordBearer(tokenUrl="token")
@@ -45,22 +39,27 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
         expire = datetime.now(timezone.utc) + timedelta(minutes=20)
 
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(
+        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
+    )
 
     return encoded_jwt
 
 
 def get_current_user(
-    token: str = Depends(oauth2_schema), db: Session = Depends(database.get_db)
+    request: Request,
+    db: Session = Depends(database.get_db),
 ):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credential",
-        headers={"WWW-Authenticate": "Bearer"},
+        headers={"X-Authentication-Error": "Cookie not found or invalid"},
     )
-
+    token = request.cookies.get("access_token")
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
