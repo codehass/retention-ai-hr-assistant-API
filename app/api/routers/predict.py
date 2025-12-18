64bacbe283c811ae43ec6f323cf46b0f5a4df025
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-12-18 16:08:03 +0100

    create predict endpoint

--- app/api/routers/predict.py
+++ app/api/routers/predict.py
@@ -1,5 +1,11 @@
 from ...schemas.user_schema import EmployeeAttritionRequest
-from fastapi import APIRouter
+from sqlalchemy.orm import Session
+from ...db.database import get_db
+from ...models.prediction_history import PredictionHistory
+from ...models.employee_attrition import EmployeeAttrition
+from ...authentication.auth import get_current_user
+from ...schemas.user_schema import UserSchema
+from fastapi import APIRouter, Depends
 import pandas as pd
 import joblib
 
@@ -14,11 +20,33 @@ def get_predict():
 
 
 @router.post("/predict-attrition/")
-async def predict_attrition(employee: EmployeeAttritionRequest):
+async def predict_attrition(
+    employee: EmployeeAttritionRequest,
+    current_user: UserSchema = Depends(get_current_user),
+    db: Session = Depends(get_db),
+):
+    employee_data = EmployeeAttrition(**employee.model_dump())
+    db.add(employee_data)
+    db.commit()
+    db.refresh(employee_data)
 
-    df = pd.DataFrame([employee.dict()])
-
-    pred = model.predict(df)
+    df = pd.DataFrame([employee.model_dump()])
+    pred = model.predict(df)[0]
     prob = model.predict_proba(df)[0][1]
 
-    return {"prediction": int(pred), "probability": round(float(prob), 4)}
+    prediction = PredictionHistory(
+        user_id=current_user.id,
+        employee_id=employee_data.id,
+        churn_probability=round(float(prob), 4),
+        attrition=int(pred),
+    )
+
+    db.add(prediction)
+    db.commit()
+    db.refresh(prediction)
+
+    return {
+        "employee_id": employee_data.id,
+        "prediction": prediction.attrition,
+        "probability": prediction.churn_probability,
+    }
