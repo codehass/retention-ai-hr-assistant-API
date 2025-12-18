d2b656787e53b436a357350951a9cf3dc11dcd94
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-12-18 14:10:00 +0100

    create predict endpoint

--- app/api/routers/predict.py
+++ app/api/routers/predict.py
@@ -1,4 +1,9 @@
+from ...schemas.user_schema import EmployeeAttritionRequest
 from fastapi import APIRouter
+import pandas as pd
+import joblib
+
+model = joblib.load("./ml/model_smote.pkl")
 
 router = APIRouter(prefix="/api/v1/predict", tags=["Prediction routes"])
 
@@ -6,3 +11,14 @@ router = APIRouter(prefix="/api/v1/predict", tags=["Prediction routes"])
 @router.get("/")
 def get_predict():
     return {"predict": "Hello from predict routes"}
+
+
+@router.post("/predict-attrition/")
+async def predict_attrition(employee: EmployeeAttritionRequest):
+
+    df = pd.DataFrame([employee.dict()])
+
+    pred = model.predict(df)
+    prob = model.predict_proba(df)[0][1]
+
+    return {"prediction": int(pred), "probability": round(float(prob), 4)}
