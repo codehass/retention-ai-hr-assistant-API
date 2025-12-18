51c16a98c0ab63fb29bce19bdd139ec8a518623d
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-12-18 16:08:37 +0100

    create prediction history

--- app/models/prediction_history.py
+++ app/models/prediction_history.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime, Boolean
-from datetime import datetime
+from datetime import datetime, timezone
+from sqlalchemy import Column, Integer, Float, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from ..db.database import Base
 
@@ -10,7 +10,10 @@ class PredictionHistory(Base):
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     churn_probability = Column(Float, nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow)
-    employee_id = Column(Integer, nullable=False)
+    attrition = Column(Integer, nullable=False)
+    created_at = Column(
+        DateTime(timezone=True), default=lambda: datetime.now(timezone.utc)
+    )
+    employee_id = Column(Integer, ForeignKey("employee_attrition.id"), nullable=False)
 
     user = relationship("User", back_populates="history")
