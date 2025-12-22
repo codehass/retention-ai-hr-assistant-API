7e531294561136c1666dafc4eeeecefb6b62ecbf
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-12-23 00:11:16 +0100

    edit eda

--- ml/ml_pipeline.py
+++ ml/ml_pipeline.py
@@ -11,7 +11,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
-
+from sklearn.svm import SVC
 
 from imblearn.over_sampling import SMOTE
 from imblearn.pipeline import Pipeline as ImbPipeline
@@ -55,7 +55,6 @@ def model_pipeline(num_list, cat_list, encoded_list, model, use_smote=False):
             ("cat", categorical_transformer, cat_list),
             ("ord", "passthrough", encoded_list),
         ],
-        # remainder="drop",
     )
 
     steps = [("preprocessor", preprocessor)]
@@ -75,12 +74,13 @@ def model_pipeline(num_list, cat_list, encoded_list, model, use_smote=False):
 def evaluate_classifier(model, X_test, y_test):
     y_pred = model.predict(X_test)
 
+    y_score = model.predict_proba(X_test)[:, 1]
     accuracy = accuracy_score(y_test, y_pred)
     precision = precision_score(y_test, y_pred, average="binary")
     recall = recall_score(y_test, y_pred, average="binary")
     f1 = f1_score(y_test, y_pred, average="binary")
 
-    return accuracy, precision, recall, f1, y_pred
+    return accuracy, precision, recall, f1, y_pred, y_score
 
 
 def train_and_evaluate(
@@ -103,8 +103,13 @@ def train_and_evaluate(
         )
     elif model_type == "randomForest":
         model = RandomForestClassifier(random_state=42, class_weight="balanced")
-    elif model_type == "gradientBoosting":
-        model = GradientBoostingClassifier(random_state=42)
+    elif model_type == "svc":
+        model = SVC(
+            kernel="rbf",
+            class_weight="balanced",
+            probability=True,
+            random_state=42,
+        )
     else:
         raise ValueError("Invalid model type")
 
@@ -127,38 +132,46 @@ def train_with_grid_search(
     already_encoded,
 ):
     param_grid_logistic = {
-        "model__C": [0.1, 1, 10],
+        "model__C": [0.01, 0.1, 1, 10],
+        "model__penalty": ["elasticnet"],
+        "model__l1_ratio": [0.2, 0.5, 0.8],
     }
 
     param_grid_rf = {
-        "model__n_estimators": [100, 200],
-        "model__max_depth": [None, 10, 20],
-        "model__min_samples_split": [2, 5],
+        "model__n_estimators": [200, 500],
+        "model__max_depth": [5, 10, 20],
+        "model__min_samples_leaf": [1, 4, 10],
+        "model__max_features": ["sqrt", "log2"],
     }
 
-    param_grid_gb = {
-        "model__n_estimators": [100, 200],
-        "model__learning_rate": [0.05, 0.1],
-        "model__max_depth": [3, 5],
+    param_grid_svc = {
+        "model__C": [0.1, 1, 10, 100],
+        "model__gamma": ["scale", "auto", 0.01, 0.1],
+        "model__kernel": ["rbf", "poly"],
     }
 
     if model_type == "logistic":
         model = LogisticRegression(
-            random_state=42,
-            max_iter=1000,
-            solver="saga",
-            l1_ratio=0,
-            class_weight="balanced",
+            random_state=42, solver="saga", max_iter=5000, class_weight="balanced"
         )
         param_grid = param_grid_logistic
 
     elif model_type == "randomForest":
-        model = RandomForestClassifier(random_state=42, class_weight="balanced")
+        model = RandomForestClassifier(
+            random_state=42,
+            class_weight="balanced",
+            n_jobs=-1,
+            min_samples_leaf=4,
+            max_features="sqrt",
+        )
+
         param_grid = param_grid_rf
 
-    elif model_type == "gradientBoosting":
-        model = GradientBoostingClassifier(random_state=42)
-        param_grid = param_grid_gb
+    elif model_type == "svc":
+        model = SVC(
+            kernel="rbf", class_weight="balanced", probability=True, random_state=42
+        )
+        param_grid = param_grid_svc
 
     else:
         raise ValueError("Invalid model type")
@@ -168,7 +181,7 @@ def train_with_grid_search(
     )
 
     grid_search = GridSearchCV(
-        pipeline, param_grid, cv=5, n_jobs=-1, scoring="accuracy"
+        pipeline, param_grid, cv=5, n_jobs=-1, scoring="average_precision"
     )
 
     grid_search.fit(X_train, y_train)
